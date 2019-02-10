48d80afbab8ffdc3875861bcd90c56b2dba9e43e
github username <github email address>
2019-02-10 10:27:34 -0500

    created db and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :ideas
+  resources :users
+  resources :issues
+  get '/', to: 'users#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
